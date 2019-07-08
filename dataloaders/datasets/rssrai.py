9f2fca912cef8cbad6e8feaf7c70fc3ab67ce09c
ArronHZG <hou.zg@foxmail.com>
2019-07-08 14:25:15 +0800

    rssrai 数据data

--- dataloaders/datasets/rssrai.py
+++ dataloaders/datasets/rssrai.py
@@ -22,23 +22,16 @@ class Rssrai(data.Dataset):
                  ):
         """
         :param base_dir: path to rssrai dataset directory
-        :param split: train/val or ["train","val"]
+        :param split: train/val
         :param transform: transform to apply
         """
         super().__init__()
         self._base_dir = base_dir
         self._image_dir = os.path.join(self._base_dir, 'img_npy')
         self._cat_dir = os.path.join(self._base_dir, 'label_npy')
-
-
-        #
-        if isinstance(split, str):
-            self.split = [split]
-        else:
-            split.sort()
-            self.split = split
-
+        self.split = split
         self.args = args
+        self.test_size = 0.1
 
         # Get list of all images from the split and check that the files exist
         self.im_ids = []
@@ -47,57 +40,50 @@ class Rssrai(data.Dataset):
 
         # 加载数据
         train_csv_url = self._base_dir + '/rssrai_train.csv'
-        test_csv_url = self._base_dir + '/rssrai_test.csv'
         data = pd.read_csv(train_csv_url)
-        data['img'].value_counts()
+        tr, vd = train_test_split(data, test_size=self.test_size, random_state=123)
+        df = None
+        if "train" == self.split:
+            df = tr
+        elif "val" == self.split:
+            df = vd
         # 切分训练集和验证集
-        tr, vd = train_test_split(data, test_size=test_size, random_state=123)
-
-        for splt in self.split:
-
-            with open(os.path.join(self._dataset_dir, splt + '.txt'), "r") as f:
-                lines = f.read().splitlines()
-
-            for name in lines:
-                _image = os.path.join(self._image_dir, name)
-                _categ= os.path.join(self._cat_dir, name)
-                assert os.path.isfile(_image)
-                assert os.path.isfile(_categ)
-                self.im_ids.append(line)
-                self.images.append(_image)
-                self.categories.append(_categ)
-
+        for row in df.itertuples(index=True, name='Pandas'):
+            _image = os.path.join(self._image_dir, getattr(row, "img"))
+            _category = os.path.join(self._cat_dir, getattr(row, "label"))
+            assert os.path.isfile(_image)
+            assert os.path.isfile(_category)
+            self.im_ids.append(getattr(row, "index"))
+            self.images.append(_image)
+            self.categories.append(_category)
         assert (len(self.images) == len(self.categories))
 
         # Display stats
         print('Number of images: {:d}'.format(len(self.images)))
 
-
     def __getitem__(self, index):
-        _img, _target = self._make_img_gt_point_pair(index)
+        _img, _target = self._read_numpy_file(index)
         sample = {'image': _img, 'label': _target}
-
-        return self.transform(sample)
+        return sample  # self.transform(sample)
 
     def __len__(self):
         return len(self.images)
 
-    def _make_img_gt_point_pair(self, index):
-        _img = Image.open(self.images[index]).convert('RGB')
-        _target = Image.fromarray(scipy.io.loadmat(self.categories[index])["GTcls"][0]['Segmentation'][0])
+    def _read_numpy_file(self, index):
+        _img = np.load(self.images[index]).astype('float64')
+        _target = np.load(self.categories[index]).astype('float64')
 
         return _img, _target
 
-    def transform(self, sample):
-        composed_transforms = transforms.Compose([
-            tr.RandomHorizontalFlip(),
-            tr.RandomScaleCrop(base_size=self.args.base_size, crop_size=self.args.crop_size),
-            tr.RandomGaussianBlur(),
-            tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-            tr.ToTensor()])
-
-        return composed_transforms(sample)
-
+    # def transform(self, sample):
+    #     composed_transforms = transforms.Compose([
+    #         tr.RandomHorizontalFlip(),
+    #         tr.RandomScaleCrop(base_size=self.args.base_size, crop_size=self.args.crop_size),
+    #         tr.RandomGaussianBlur(),
+    #         tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    #         tr.ToTensor()])
+    #
+    #     return composed_transforms(sample)
 
     def __str__(self):
         return 'SBDSegmentation(split=' + str(self.split) + ')'
@@ -114,28 +100,30 @@ if __name__ == '__main__':
     args.base_size = 513
     args.crop_size = 513
 
-    sbd_train = SBDSegmentation(args, split='train')
-    dataloader = DataLoader(sbd_train, batch_size=2, shuffle=True, num_workers=2)
+    train = Rssrai(args, split='train')
+    val = Rssrai(args, split='val')
+    # data_loader = DataLoader(sbd_train, batch_size=2, shuffle=True, num_workers=2)
+    # print(data_loader)
 
-    for ii, sample in enumerate(dataloader):
-        for jj in range(sample["image"].size()[0]):
-            img = sample['image'].numpy()
-            gt = sample['label'].numpy()
-            tmp = np.array(gt[jj]).astype(np.uint8)
-            segmap = decode_segmap(tmp, dataset='pascal')
-            img_tmp = np.transpose(img[jj], axes=[1, 2, 0])
-            img_tmp *= (0.229, 0.224, 0.225)
-            img_tmp += (0.485, 0.456, 0.406)
-            img_tmp *= 255.0
-            img_tmp = img_tmp.astype(np.uint8)
-            plt.figure()
-            plt.title('display')
-            plt.subplot(211)
-            plt.imshow(img_tmp)
-            plt.subplot(212)
-            plt.imshow(segmap)
-
-        if ii == 1:
-            break
-
-    plt.show(block=True)
+    # for ii, sample in enumerate(dataloader):
+    #     for jj in range(sample["image"].size()[0]):
+    #         img = sample['image'].numpy()
+    #         gt = sample['label'].numpy()
+    #         tmp = np.array(gt[jj]).astype(np.uint8)
+    #         # segmap = decode_segmap(tmp, dataset='pascal')
+    #         # img_tmp = np.transpose(img[jj], axes=[1, 2, 0])
+    #         # img_tmp *= (0.229, 0.224, 0.225)
+    #         # img_tmp += (0.485, 0.456, 0.406)
+    #         # img_tmp *= 255.0
+    #         # img_tmp = img_tmp.astype(np.uint8)
+    #         plt.figure()
+    #         plt.title('display')
+    #         plt.subplot(211)
+    #         plt.imshow(img_tmp)
+    #         plt.subplot(212)
+    #         plt.imshow(segmap)
+    #
+    #     if ii == 1:
+    #         break
+    #
+    # plt.show(block=True)
